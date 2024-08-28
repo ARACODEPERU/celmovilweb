6dd7c268dfab4ffb07e5988a40df4180fe323aa2
Yisus Web <connexion.jesus@gmail.com>
2024-08-27 19:03:35 -0500

    fgg

--- resources/views/pages/producto-descripcion.blade.php
+++ resources/views/pages/producto-descripcion.blade.php
@@ -28,7 +28,7 @@
                                     <img src="{{ asset($product->image) }}" alt="{{ $product->name }}" />
                                 </a>
                                 @if ($product->existance==0)
-                                    <span>AGOTADO</span>
+                                    <span style="background: red; padding: 15px 25px; color: #fff; position:absolute; margin-top: -50px;">AGOTADO</span>
                                 @endif
                             </div>
                             @foreach ($product->images as $image)
