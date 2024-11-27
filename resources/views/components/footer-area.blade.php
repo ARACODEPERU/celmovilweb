2738d998aa130f5a17b0482856d2b33e6f2858a8
Yisus Web <connexion.jesus@gmail.com>
2024-11-26 20:26:54 -0500

    wtsapp

--- resources/views/components/footer-area.blade.php
+++ resources/views/components/footer-area.blade.php
@@ -15,9 +15,79 @@
         <img src="{{ $redesSociales[6]->content }}" alt="Tiktok">
     </a>-->
 
-    <a class="appWhatsapp" target="_blanck" href="https://api.whatsapp.com/send?phone=51{{ $redesSociales[9]->content }}&text=Hola&nbsp;CelMovil!&nbsp;me&nbsp;pueden&nbsp;ayudar?">
+    {{-- <a class="appWhatsapp" target="_blanck" href="https://api.whatsapp.com/send?phone=51{{ $redesSociales[9]->content }}&text=Hola&nbsp;CelMovil!&nbsp;me&nbsp;pueden&nbsp;ayudar?">
         <img src="{{ $redesSociales[8]->content }}" alt="Whatsapp">
-    </a>
+    </a> --}}
+
+    
+    
+    <div id="whatsapp">
+        <a href="https://api.whatsapp.com/send?phone=51{{ $redesSociales[9]->content }}&text=Hola&nbsp;CelMovil!&nbsp;me&nbsp;pueden&nbsp;ayudar?" 
+            target="_blanck" 
+            class="wtsapp" 
+            data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <i class="fa fa-whatsapp"></i>
+        </a>
+    </div>
+
+    <style type="text/css">
+        #whatsapp .wtsapp{
+            position: fixed;
+            transform: all .5s ease;
+            background-color: #25D366;
+            display: block;
+            text-align: center;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+            border-radius: 50px;
+            border-right: none;
+            color: #fff;
+            font-weight: 700;
+            font-size: 30px;
+            bottom: 40px;
+            right: 20px;
+            border: 0;
+            z-index: 9999;
+            width: 50px;
+            height: 50px;
+            line-height: 50px;
+        }
+
+        #whatsapp .wtsapp:before{
+            content: "";
+            position: absolute;
+            z-index: -1;
+            left: 50%;
+            top: 50%;
+            transform: translateX(-50%) translateY(-50%);
+            display: block;
+            width: 60px;
+            height: 60px;
+            background-color: #25D366;
+            border-radius: 50%;
+            -webkit-animation: pulse-border 1500ms ease-out infinite;
+            animation: pulse-border 1500ms ease-out infinite;
+        }
+
+        #whatsapp .wtsapp:focus{
+            border: none;
+            outline: none;
+        }
+
+        @keyframes pulse-border{
+            0%{
+                transform: translateX(-50%) translateY(-50%) translateZ(0) scale(1);
+                opacity: 1;
+            }
+            100%{
+                transform: translateX(-50%) translateY(-50%) translateZ(0) scale(1.5);
+                opacity: 0;
+            }
+        }
+
+    </style>
+
+
+
     <!-- footer section start -->
     <footer>
         <!-- brand logo area start -->
