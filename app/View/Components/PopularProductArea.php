cd0111812a59dc144aa26708a88f9db952d158ff
José Carlos <60234854+Grandejosh@users.noreply.github.com>
2024-12-06 17:16:30 -0500

    d
    d

--- app/View/Components/PopularProductArea.php
+++ app/View/Components/PopularProductArea.php
@@ -17,7 +17,10 @@ class PopularProductArea extends Component
     {
         $this->products = OnliItem::with('product')->where('existence', 1)
                                     ->where('discount', '>', 0) // para buscar solo con descuentos
-                                    ->inRandomOrder()->limit(15)->get();
+                                    ->inRandomOrder()
+                                    ->limit(15)
+                                    ->distinct()
+                                    ->get();
 
         $this->product_popular_area = CmsSection::where('component_id', 'productos_populares_area_11')
             ->join('cms_section_items', 'section_id', 'cms_sections.id')
